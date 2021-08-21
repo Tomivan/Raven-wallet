5314c9557d7e9ff8f487e8cf3471dd33b41dba70
Tomivan <gracejunkie20@gmail.com>
2021-08-21 15:42:30 +0100

    project setup

---
+++ src/pages/Transfer/transfer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './transfer.css';
+
+const Transfer = () => {
+   return(
+       <div></div>
+   ) 
+}
+
+export default Transfer;
---
+++ src/pages/Withdrawal/withdraw.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './withdrawal.css';
+
+const Withdrawal = () => {
+   return(
+       <div></div>
+   ) 
+}
+
+export default Withdrawal;
